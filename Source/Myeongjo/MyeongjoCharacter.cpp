08b9049fbbd5e8685afe1a307e04a966b41325e6
pch2001 <pencil0303@naver.com>
2026-04-26 23:50:01 +0900

    1차 수정
    맵 일부분 협곡

--- Source/Myeongjo/MyeongjoCharacter.cpp
+++ Source/Myeongjo/MyeongjoCharacter.cpp
@@ -35,6 +35,9 @@ AMyeongjoCharacter::AMyeongjoCharacter()
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
 
+	GetCharacterMovement()->MaxWalkSpeed = 10000.f;
+
+
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
